77a9c75affd49f9dbc1fceacb0a215145eeed3a7
yhc <2580577036@qq.com>
2024-09-10 09:00:45 +0800

    feat:model init load

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,97 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div></div>  
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup>
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
+import { Water } from "three/examples/jsm/objects/Water2";
+import gsap from "gsap";
+import { ref } from "vue";
+
+
+// 初始化场景
+const scene = new THREE.Scene();
+// 初始化相机
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.set(-3.23, 2.98, 4.06);
+camera.updateProjectionMatrix();
+// 初始化渲染器
+const renderer = new THREE.WebGLRenderer({
+  // 设置抗锯齿
+  antialias: true,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
+
+// 初始化控制器
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(-8, 2, 0);
+controls.enableDamping = true;
+
+// 初始化loader
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath("./draco/");
+const gltfLoader = new GLTFLoader();
+gltfLoader.setDRACOLoader(dracoLoader);
+
+
+// 加载模型
+gltfLoader.load("./model/scene.glb", (gltf) => {
+  const model = gltf.scene;
+  model.traverse((child) => {
+    if (child.name === "Plane") {
+      child.visible = false;
+    }
+    if (child.isMesh) {
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+  scene.add(model);
+});
+
+
+// 添加平行光
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 50, 0);
+scene.add(light);
+
+// 添加点光源
+const pointLight = new THREE.PointLight(0xffffff, 50);
+pointLight.position.set(0.1, 2.4, 0);
+pointLight.castShadow = true;
+scene.add(pointLight);
+
+function render() {
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+  controls.update();
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+render();
+
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+canvas {
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  left: 0;
+  top: 0;
 }
 </style>
